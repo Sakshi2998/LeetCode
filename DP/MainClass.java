9b5d380c0531668fd2891a13ae487868d28182aa
sakshi2998 <sakshikatkar10@gmail.com>
2020-02-17 11:10:25 +0530

    first commit

---
+++ DP/MainClass.java
@@ -0,0 +1,14 @@
+package DP;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		LongestCommonSubstring lcs = new LongestCommonSubstring();
+		
+		System.out.println(lcs.longestCommonSubsequence("AGGTAB", "GXTXAYB"));
+		
+		
+	}
+
+}
