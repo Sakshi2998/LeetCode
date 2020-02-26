cccc4b465bbe691630fc51189c14c0b81db573d3
sakshi2998 <sakshikatkar10@gmail.com>
2020-02-26 10:45:29 +0530

    modified 'FindFirstAndLastElementSortedArray'

--- array/FirstAndLastElementSortedArray.java
+++ array/FirstAndLastElementSortedArray.java
@@ -1,42 +1,42 @@
 package array;
 
 public class FirstAndLastElementSortedArray {
-	
+
 	public int[] searchRange(int[] nums, int target) {
 		int res[] = new int[2];
-		res[0] = searchFirst(0, nums.length, nums, target);
-		res[1] = searchLast(0, nums.length, nums, target);
+		res[0] = search(0, nums.length, nums, target, true);
+		res[1] = search(0, nums.length, nums, target, false);
 		return res;
 	}
 
-	int searchFirst(int lb, int ub, int[] nums, int target) {
-		if (lb >= ub) {
-			return -1;
-		}
-		int mid = (lb + ub) / 2;
-		if ((nums[mid] == target && mid == 0) || (nums[mid] == target && nums[mid - 1] != target)) {
-			return mid;
-		}
-		if (target > nums[mid]) {
-			return searchFirst(mid + 1, ub, nums, target);
-		}
-		return searchFirst(lb, mid, nums, target);
-	}
+	int search(int lb, int ub, int[] nums, int target, boolean left) {
 
-	int searchLast(int lb, int ub, int[] nums, int target) {
-		if (lb >= ub) {
+		if (lb >= ub)
 			return -1;
-		}
 
 		int mid = (lb + ub) / 2;
 
-		if ((nums[mid] == target && mid == nums.length - 1) || (nums[mid] == target && nums[mid + 1] != target)) {
-			return mid;
+		if (nums[mid] == target) {
+			if (left && (mid == 0 || nums[mid - 1] != target))
+				return mid;
+
+			if (!left && (mid == nums.length - 1 || nums[mid + 1] != target))
+				return mid;
 		}
 
-		if (target >= nums[mid]) {
-			return searchLast(mid + 1, ub, nums, target);
+		if (left) {
+			if (target <= nums[mid]) {
+				return search(lb, mid, nums, target, left);
+			} else {
+				return search(mid + 1, ub, nums, target, left);
+			}
+		} else {
+			if (target < nums[mid]) {
+				return search(lb, mid, nums, target, left);
+			} else {
+				return search(mid + 1, ub, nums, target, left);
+			}
 		}
-		return searchLast(lb, mid, nums, target);
+
 	}
 }
--- array/MainClass.java
+++ array/MainClass.java
@@ -4,8 +4,8 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		RemoveDuplicatesFromSortedArray r = new RemoveDuplicatesFromSortedArray();
-		System.out.println(r.removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4}));
+		FirstAndLastElementSortedArray r = new FirstAndLastElementSortedArray();
+		System.out.println(r.searchRange(new int[] {0,0,0},0));
 	}
 
 }
