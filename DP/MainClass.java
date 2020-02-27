d3e4514f163e322213dc258176dc0fb22ee91d78
sakshi2998 <sakshikatkar10@gmail.com>
2020-02-27 11:42:43 +0530

    added 'CoinChange2'

--- DP/MainClass.java
+++ DP/MainClass.java
@@ -3,7 +3,8 @@ package dp;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+		CoinChange2 cc = new  CoinChange2();
+		System.out.println(cc.change(5, new int[] {1,2,5}));
 		
 	}
 
