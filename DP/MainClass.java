87c9fd38ea49ae300d0a65bc467846094331f84c
sakshi2998 <sakshikatkar10@gmail.com>
2020-02-27 18:15:05 +0530

    modified 'MinCostClimbingStairs'

--- DP/MainClass.java
+++ DP/MainClass.java
@@ -4,9 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		MinCostClimbingStairs mcs = new MinCostClimbingStairs();
-		int[] cost = {1};
-		System.out.println(mcs.minCostClimbingStairs(cost));
+		
 	}
 
 }
