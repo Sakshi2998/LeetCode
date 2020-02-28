637f462a2a540224ddcdd05d53494e53f3e1446e
sakshi2998 <sakshikatkar10@gmail.com>
2020-02-28 10:44:06 +0530

    added 'MinimumPathSum'

--- DP/MainClass.java
+++ DP/MainClass.java
@@ -4,9 +4,11 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		UniquePathsII up = new UniquePathsII();
-		int[][] og = {{0}};
-		System.out.println(up.uniquePathsWithObstacles(og));
+		MinimumPathSum up = new MinimumPathSum();
+		int[][] og = {{1,3,1},
+				  {1,5,1},
+				  {4,2,1}};
+		System.out.println(up.minPathSum(og));
 	}
 
 }
