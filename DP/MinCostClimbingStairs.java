312dace77ad134b2991716305d860e6878620c14
sakshi2998 <sakshikatkar10@gmail.com>
2020-02-28 09:23:40 +0530

    added 'UniquePaths'

--- DP/MinCostClimbingStairs.java
+++ DP/MinCostClimbingStairs.java
@@ -2,19 +2,9 @@ package dp;
 
 public class MinCostClimbingStairs {
 	public int minCostClimbingStairs(int[] cost) {
-
-		if(cost.length==0)
-			return 0;
-	
-		int temp =cost[cost.length-1];
-		for(int i=2;i<cost.length;i++) {
-			
-			cost[i]+=Integer.min(cost[i-1], cost[i-2]);
-			
-			
+		for (int i = 2; i < cost.length; i++) {
+			cost[i] += Integer.min(cost[i - 1], cost[i - 2]);
 		}
-		
-		return Integer.min(cost[cost.length-1],cost[cost.length-2]);
-		
+		return Integer.min(cost[cost.length - 1], cost[cost.length - 2]);
 	}
 }
