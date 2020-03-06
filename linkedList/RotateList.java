909a42f06b5709654ee8119013ceff57fb21dcdf
sakshi2998 <sakshikatkar10@gmail.com>
2020-03-06 11:47:58 +0530

    added 'MaximumDepthofBinaryTree'

--- linkedList/RotateList.java
+++ linkedList/RotateList.java
@@ -1,32 +1,27 @@
 package linkedList;
 
 public class RotateList {
-	public ListNode rotateRight(ListNode head, int k) {
 
-		int length=1;
+	public ListNode rotateRight(ListNode head, int k) {
+		if (head == null || head.next == null)
+			return head;
+		int length = 1;
 		ListNode end = head;
-		while(end.next!=null)
-		{
-			
+		while (end.next != null) {
+
 			length++;
-			end=end.next;
+			end = end.next;
 		}
-		
-		k=length-(k%length);
-		System.out.println(k);
-		
+
+		k = length - (k % length) - 1;
 		ListNode part = head;
-		for(int i=0;i<k;i++)
-			part=part.next;
-		
-		end.next=head;
-		head=part.next;
+		for (int i = 0; i < k; i++)
+			part = part.next;
+
+		end.next = head;
+		head = part.next;
 		part.next = null;
-		
-	
-		
-		System.out.println(part.val);
+
 		return head;
-			
 	}
 }
---
+++ tree/binaryTree/MaximumDepthofBinaryTree.java
@@ -0,0 +1,15 @@
+package tree.binaryTree;
+
+import tree.TreeNode;
+
+public class MaximumDepthofBinaryTree {
+	
+//Recursive
+	
+	public int maxDepth(TreeNode root) {
+		if(root==null)
+			return 0;
+		else return 1 + (Math.max(maxDepth(root.left),maxDepth(root.right))); 
+	}
+
+}
