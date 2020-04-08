6ff816129205409aea44e43ae4178a93a2cff523
Sakshi Katkar <sakshikatkar10@gmail.com>
2020-04-08 13:21:52 +0530

    added 'MiddleoftheLinkedList'

---
+++ linkedList/MiddleoftheLinkedList.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+public class MiddleoftheLinkedList {
+	public ListNode middleNode(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+}
