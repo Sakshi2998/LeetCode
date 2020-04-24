5b93790ae55a3429cd35650897f9f886b38ffa96
Sakshi Katkar <sakshikatkar10@gmail.com>
2020-04-24 12:19:29 +0530

    added 'BitwiseANDofNumbersRange'

---
+++ bitManipulation/BitwiseANDofNumbersRange.java
@@ -0,0 +1,13 @@
+package bitManipulation;
+
+public class BitwiseANDofNumbersRange {
+	public int rangeBitwiseAnd(int m, int n) {
+		int i = 0;
+		while (m != n) {
+			m = m >> 1;
+			n = n >> 1;
+			i++;
+		}
+		return m << i;
+	}
+}
