7354b8a85b02a35ad8adb86f32e14bbbce555dc5
Sakshi Katkar <sakshikatkar10@gmail.com>
2020-05-08 14:01:27 +0530

    added 'CheckIfItIsaStraightLine'

---
+++ math/CheckIfItIsaStraightLine.java
@@ -0,0 +1,20 @@
+package math;
+
+public class CheckIfItIsaStraightLine {
+	public boolean checkStraightLine(int[][] coordinates) {
+
+		if (coordinates.length <= 2)
+			return true;
+		float slope = (float) (coordinates[1][1] - coordinates[0][1]) / (float) (coordinates[1][0] - coordinates[0][0]);
+		for (int i = 2; i < coordinates.length; i++) {
+			float newSlope = (float) (coordinates[i][1] - coordinates[i - 1][1])
+					/ (float) (coordinates[i][0] - coordinates[i - 1][0]);
+
+			if (newSlope != slope)
+				return false;
+
+		}
+		return true;
+
+	}
+}
