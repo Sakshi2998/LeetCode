94ae320da87376d9dcf0bdf98ad1ea35675d11ac
Sakshi Katkar <sakshikatkar10@gmail.com>
2020-06-01 18:07:52 +0530

    added 'InvertBinaryTree'

---
+++ tree/binaryTree/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+package tree.binaryTree;
+
+public class InvertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null)
+			return null;
+
+		TreeNode temp = invertTree(root.left);
+		root.left = invertTree(root.right);
+		root.right = temp;
+
+		return root;
+	}
+}
