896ad6204ebe6c2bbcfb5a3503071a0af8ec0c9d
Sakshi Katkar <sakshikatkar10@gmail.com>
2021-10-10 11:14:48 +0530

    Create BinarySearch.java

---
+++ array/BinarySearch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+
+    }
+
+    int binarySearch(int[] nums, int start, int end, int target) {
+        if (start > end)
+            return -1;
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target)
+            return mid;
+        if (nums[mid] > target)
+            return binarySearch(nums, start, mid - 1, target);
+        else
+            return binarySearch(nums, mid + 1, end, target);
+    }
+}
