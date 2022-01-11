5f1935a396af39bc56d0f988ec0c9d8c31338237
Sakshi Katkar <sakshikatkar10@gmail.com>
2022-01-11 17:25:54 +0530

    Update SumOfRootToLeafBinaryNumbers.java

--- tree/SumOfRootToLeafBinaryNumbers.java
+++ tree/SumOfRootToLeafBinaryNumbers.java
@@ -17,15 +17,13 @@ class Solution {
     public int sumRootToLeaf(TreeNode root) {
         return calculate(root, 0);  
     }
-    
+ 
     public int calculate(TreeNode root, int sum){  
         if(root==null)
             return 0;
         sum = sum*2+root.val;
-        
         if(root.left==null && root.right==null)
-            return sum;
-            
+            return sum;    
         return(calculate(root.left, sum) + calculate(root.right, sum));
     }
 }
