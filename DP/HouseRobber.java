8f86eea902c974ad8db54fcd387f989ee4e14fae
Sakshi Katkar <sakshikatkar10@gmail.com>
2022-01-13 12:39:52 +0530

    Update HouseRobber.java

--- DP/HouseRobber.java
+++ DP/HouseRobber.java
@@ -1,18 +1,9 @@
-package dp;
-
-public class HouseRobber {
-	public int rob(int[] nums) {
-
-		if (nums.length == 0)
-			return 0;
-		if (nums.length < 2) {
-			return nums[0];
-		}
-		nums[1] = Integer.max(nums[0], nums[1]);
-		for (int i = 2; i < nums.length; i++) {
-			nums[i] = Math.max(nums[i] + nums[i - 2], nums[i - 1]);
-		}
-		return Integer.max(nums[nums.length - 2], nums[nums.length - 1]);
-
-	}
+class Solution {
+    public int rob(int[] nums) {
+       if(nums.length>1)
+           nums[1] = Math.max(nums[0], nums[1]);
+       for(int i = 2; i<nums.length; i++) 
+            nums[i] = Math.max(nums[i-1], nums[i-2]+nums[i]);
+       return nums[nums.length-1];
+    }
 }
